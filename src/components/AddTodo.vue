20ed10295b823d16b0f3249ab7f2b449ed21c7ff
Yacine <ybouakkaz@gmail.com>
2020-03-01 21:08:41 -0500

    Initial commit Todo list app

---
+++ src/components/AddTodo.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <form @submit="addTodo">
+            <input type="text" v-model="title" name="title" placeholder="Add todo..." >
+            <input type="submit" value="Submit" class="btn">
+        </form>
+    </div>
+</template>
+
+<script>
+import uuid from 'uuid';
+
+export default {
+    name: "AddTodo",
+    data() {
+        return {
+            title: ''
+        }
+    },
+    methods: {
+        addTodo(e) {
+            e.preventDefault();
+            const newTodo = {
+                id: uuid.v4(),
+                title: this.title,
+                completed: false
+            }
+            //send up to parent
+            this.$emit('add-todo', newTodo);
+
+            this.title = '';
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+
+</style>
---
+++ src/components/layout/Header.vue
@@ -0,0 +1,17 @@
+<template>
+    <header class="header">
+        <h1>Todo List</h1>
+    </header>
+</template>
+
+<script>
+
+export default {
+    name: "Header"    
+}
+</script>
+
+<style scoped>
+
+</style>
+
